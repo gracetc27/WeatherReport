1abcc357bed78060c85eb5f1f96f7051cbdbaf9c
Grace Couch <gracetcouch@gmail.com>
2025-05-16 17:38:52 +0100

    example url

---
+++ WeatherReport/Services/OpenWeatherMapAPI.swift
@@ -0,0 +1,12 @@
+//
+//  OpenWeatherMapAPI.swift
+//  WeatherReport
+//
+//  Created by Grace couch on 16/05/2025.
+//
+
+import Foundation
+
+class OpenWeatherMapAPI {
+    let url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=INSERTAPIKEYHERE"
+}
