df6800d814bc93bcf4dd48904e57b542b3fd5683
Grace Couch <gracetcouch@gmail.com>
2025-05-16 17:00:55 +0100

    initial commit

---
+++ WeatherReport/WeatherReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherReportApp.swift
+//  WeatherReport
+//
+//  Created by Grace couch on 16/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherReportTests/WeatherReportTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherReportTests.swift
+//  WeatherReportTests
+//
+//  Created by Grace couch on 16/05/2025.
+//
+
+import Testing
+@testable import WeatherReport
+
+struct WeatherReportTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
