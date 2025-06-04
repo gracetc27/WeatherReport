05518726dbfe9a2a01a7acb2991e84448acdbb0f
Grace Couch <gracetcouch@gmail.com>
2025-06-04 14:36:24 +0100

    precipitation panel

---
+++ WeatherReport/Views/Components/PrecipitationPanel.swift
@@ -0,0 +1,38 @@
+//
+//  PrecipitationPanel.swift
+//  WeatherReport
+//
+//  Created by Grace couch on 04/06/2025.
+//
+
+import SwiftUI
+
+struct PrecipitationPanel: View {
+    let rain: Rain?
+    let clouds: Clouds
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Precipitation")
+                .font(.title2.bold())
+                .padding(.vertical)
+            HStack {
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Rain per hour:")
+                    Text("Cloud cover:")
+                }
+                .bold()
+                Spacer()
+                VStack(alignment: .trailing, spacing: 20) {
+                    Text("\(rain?.perHour ?? 0)mm")
+                    Text("\(clouds.all)%")
+                }
+            }
+        }
+        .frame(width: screenWidth * 0.9)
+
+    }
+}
+
+#Preview {
+    PrecipitationPanel(rain: Weather.defaultWeather.rain, clouds: Weather.defaultWeather.clouds)
+}
