00f97c4f2cf368941fb95398e3c036315dbfcac2
Grace Couch <gracetcouch@gmail.com>
2025-06-24 16:42:48 +0100

    adds get weather success and failure tests for home view model

--- WeatherReportTests/HomeViewModelTests.swift
+++ WeatherReportTests/HomeViewModelTests.swift
@@ -29,8 +29,64 @@ struct HomeViewModelTests {
 
         #expect(sut.recentPlace == nil)
         #expect(sut.recentPlaceError == .loadingFailed)
+        #expect(sut.isShowingPlaceError == true)
+    }
+
+    @Test func getWeatherUpdatesRecentWeatherSuccess() async throws {
+        let testPlaceManager = FakePlaceManager(loadSavedPlaceResult: .success(.defaultPlace))
+        let sut = HomeViewModel(placeManager: testPlaceManager, service: FakeWeatherServiceSuccess())
+        await sut.loadRecentPlace()
+
+        #expect(sut.recentWeather == nil)
+
+        let expectedWeather = Weather(
+            coord: APICoord(lon: 250.1, lat: 50.7),
+            weather: [APIWeatherIcon(
+                id: 1,
+                main: "weather icon 1",
+                description: "rain",
+                icon: "10d")],
+            main: MainWeather(
+                temp: 10.1,
+                feelsLike: 12.2,
+                tempMin: 5.6,
+                tempMax: 14.5,
+                pressure: 3,
+                humidity: 4),
+            wind: Wind(
+                speed: 20.2,
+                deg: 30,
+                gust: 23.7),
+            clouds: APIClouds(
+                all: 63),
+            rain: Rain(
+                perHour: 2.73)
+        )
+
+        await sut.getWeatherForRecentPlace()
+
+        #expect(sut.recentWeather == expectedWeather)
+    }
+
+    @Test func getWeatherUpdatesRecentWeatherFailure() async throws {
+        let testPlaceManager = FakePlaceManager(loadSavedPlaceResult: .success(.defaultPlace))
+        let sut = HomeViewModel(placeManager: testPlaceManager, service: FakeWeatherServiceFailure())
+        await sut.loadRecentPlace()
+
+        #expect(sut.recentWeather == nil)
+
+        await sut.getWeatherForRecentPlace()
+
+        #expect(sut.recentWeather == nil)
+
+        #expect(sut.weatherError != nil)
+        #expect(sut.isShowingWeatherError == true)
     }
 }
+
+
+
+
 
 class FakePlaceManager: PlaceManagerProtocol {
     let loadSavedPlaceResult: Result<Coordinate, SaveLoadError>
