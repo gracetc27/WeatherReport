94294ab6c0fa7302811e98aea240f1542aa56ee2
Grace Couch <gracetcouch@gmail.com>
2025-06-27 14:18:42 +0100

    search places vm search places success and failure tests

--- WeatherReportTests/SearchPlacesViewModelTests.swift
+++ WeatherReportTests/SearchPlacesViewModelTests.swift
@@ -7,11 +7,35 @@
 
 import Testing
 @testable import WeatherReport
+import Foundation
 
 struct SearchPlacesViewModelTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test func searchPlacesUpdatesPlaceSuccess() async throws {
+        let fakePlaceManager = FakePlaceManager(loadSavedPlaceResult: .success(.defaultPlace))
+        let fakeSuccessService = FakeWeatherServiceSuccess()
+        let sut = SearchPlacesViewModel(placeManager: fakePlaceManager, service: fakeSuccessService)
+
+        #expect(sut.place == nil)
+
+        sut.searchText = "London"
+
+        await sut.searchPlaces()
+        #expect(sut.place?.name == "London")
     }
 
+    @Test func searchPlacesUpdatesPlaceFailure() async throws {
+        let fakePlaceManager = FakePlaceManager(loadSavedPlaceResult: .success(.defaultPlace))
+        let fakeFailureService = FakeWeatherServiceFailure()
+        let sut = SearchPlacesViewModel(placeManager: fakePlaceManager, service: fakeFailureService)
+        
+        #expect(sut.place == nil)
+        
+        sut.searchText = "London"
+
+        await sut.searchPlaces()
+
+        #expect(sut.place == nil)
+        #expect(sut.placeError == .decodingFailed)
+    }
 }
