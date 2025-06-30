14a649d6107f1060eea072a69c2bd3608e9864a4
Grace Couch <gracetcouch@gmail.com>
2025-06-30 16:11:48 +0100

    adding spy for saving tests

--- WeatherReportTests/FakePlaceManager.swift
+++ WeatherReportTests/FakePlaceManager.swift
@@ -8,15 +8,26 @@
 import Foundation
 
 class FakePlaceManager: PlaceManagerProtocol {
+    var saveCallCount = 0
+    var testSavePlace: Coordinate?
     let loadSavedPlaceResult: Result<Coordinate, SaveLoadError>
+    let savePlaceResult: Result<Void, SaveLoadError>
 
-    init(loadSavedPlaceResult: Result<Coordinate, SaveLoadError>) {
+    init(
+        loadSavedPlaceResult: Result<Coordinate, SaveLoadError> = .success(.defaultPlace),
+        savePlaceResult: Result<Void, SaveLoadError> = .success(())
+    ) {
         self.loadSavedPlaceResult = loadSavedPlaceResult
+        self.savePlaceResult = savePlaceResult
     }
 
     func loadSavedPlace() async throws(SaveLoadError) -> Coordinate {
         try loadSavedPlaceResult.get()
     }
 
-    func savePlace(_ place: Coordinate) throws(SaveLoadError) {}
+    func savePlace(_ place: Coordinate) throws(SaveLoadError) {
+        saveCallCount += 1
+        testSavePlace = place
+        try savePlaceResult.get()
+    }
 }
