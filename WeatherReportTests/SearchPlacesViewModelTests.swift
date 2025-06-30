8b2c2b0d78ee70139caa7bbd85c3e307b4a7f14b
Grace Couch <gracetcouch@gmail.com>
2025-06-30 16:12:45 +0100

    getWeather success and failure tests, savePlace success and failure tests

--- WeatherReportTests/SearchPlacesViewModelTests.swift
+++ WeatherReportTests/SearchPlacesViewModelTests.swift
@@ -28,14 +28,92 @@ struct SearchPlacesViewModelTests {
         let fakePlaceManager = FakePlaceManager(loadSavedPlaceResult: .success(.defaultPlace))
         let fakeFailureService = FakeWeatherServiceFailure()
         let sut = SearchPlacesViewModel(placeManager: fakePlaceManager, service: fakeFailureService)
-        
+
         #expect(sut.place == nil)
-        
+
         sut.searchText = "London"
 
         await sut.searchPlaces()
 
         #expect(sut.place == nil)
         #expect(sut.placeError == .decodingFailed)
+    }
+
+    @Test func getWeatherUpdatesWeatherSuccess() async throws {
+        let fakePlaceManager = FakePlaceManager(loadSavedPlaceResult: .success(.defaultPlace))
+        let fakeSuccessService = FakeWeatherServiceSuccess()
+        let sut = SearchPlacesViewModel(placeManager: fakePlaceManager, service: fakeSuccessService)
+
+        #expect(sut.weather == nil)
+        sut.place = .defaultPlace
+
+        let expectedWeather = Weather(
+            coord: APICoord(lon: 250.1, lat: 50.7),
+            weather: [APIWeatherIcon(
+                id: 1,
+                main: "weather icon 1",
+                description: "rain",
+                icon: "10d")],
+            main: MainWeather(
+                temp: 10.1,
+                feelsLike: 12.2,
+                tempMin: 5.6,
+                tempMax: 14.5,
+                pressure: 3,
+                humidity: 4),
+            wind: Wind(
+                speed: 20.2,
+                deg: 30,
+                gust: 23.7),
+            clouds: APIClouds(
+                all: 63),
+            rain: Rain(
+                perHour: 2.73)
+        )
+
+        await sut.getWeatherForCoord()
+
+        #expect(sut.weather == expectedWeather)
+
+    }
+
+    @Test func getWeatherUpdatesWeatherFailure() async throws {
+        let fakePlaceManager = FakePlaceManager(loadSavedPlaceResult: .success(.defaultPlace))
+        let fakeFailureService = FakeWeatherServiceFailure()
+        let sut = SearchPlacesViewModel(placeManager: fakePlaceManager, service: fakeFailureService)
+
+        #expect(sut.weather == nil)
+
+        sut.place = .defaultPlace
+
+        await sut.getWeatherForCoord()
+
+        #expect(sut.weather == nil)
+        #expect(sut.weatherError == .decodingFailed)
+    }
+
+    @Test func saveRecentPlaceSuccess() async throws {
+        let fakePlaceManager = FakePlaceManager(loadSavedPlaceResult: .success(.defaultPlace), savePlaceResult: .success(()))
+        let fakeSuccessService = FakeWeatherServiceSuccess()
+        let sut = SearchPlacesViewModel(placeManager: fakePlaceManager, service: fakeSuccessService)
+        
+        sut.place = .defaultPlace
+
+        sut.saveRecentPlace()
+
+        #expect(fakePlaceManager.saveCallCount == 1)
+        #expect(fakePlaceManager.testSavePlace == .defaultPlace)
+    }
+
+    @Test func saveRecentPlaceFailure() async throws {
+        let fakePlaceManager = FakePlaceManager(loadSavedPlaceResult: .failure(.savingFailed), savePlaceResult: .failure(.savingFailed))
+        let fakeSuccessService = FakeWeatherServiceSuccess()
+        let sut = SearchPlacesViewModel(placeManager: fakePlaceManager, service: fakeSuccessService)
+
+        sut.place = .defaultPlace
+
+        sut.saveRecentPlace()
+
+        #expect(sut.saveError == .savingFailed)
     }
 }
